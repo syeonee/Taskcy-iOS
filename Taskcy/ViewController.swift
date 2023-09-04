14c1852f5d7d8e9fc269308b33d1bac6f3dc0837
syeonee <syeoneee326@gmail.com>
2023-09-04 23:56:49 +0900

    Initial Commit

---
+++ Taskcy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Taskcy
+//
+//  Created by 김시연 on 2023/09/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
